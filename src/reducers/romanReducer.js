5658379a20f4791a721ee970736b72622a315f94
Toni Silfver <toni.silfver@protonmail.ch>
2023-02-24 10:57:38 +0200

    v4 fin Step 3. fin Step 4.

--- src/reducers/romanReducer.js
+++ src/reducers/romanReducer.js
@@ -1,9 +1,19 @@
 export default function romanReducer(state, action) {
   switch (action.type) {
     case "ADD-ROMAN":
+      let convertedNumber = "";
+      if (action.number.toLowerCase() === "i") {
+        convertedNumber = 1
+      }
+      if (convertedNumber !== "") {
+        return [
+          ...state,
+          { number: `${action.number} is ${convertedNumber}`, id: action.id },
+        ];
+      }
       return [
         ...state,
-        { number: action.number, id: action.id },
+        { number: `Your input was "${action.number}"`, id: action.id },
       ];
     case "REMOVE-ROMAN":
       return state.filter((roman) => roman.id !== action.id);
